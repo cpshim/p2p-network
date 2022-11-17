8b2fd01c2b6a199b3db1b579ffdaa6ee61610e7e
cshim <chris.shim1999@hotmail.com>
2022-11-17 17:40:13 -0500

    Implemented static database with content limit

--- index_server.c
+++ index_server.c
@@ -24,7 +24,8 @@ int main(int argc, char *argv[])
 		char peerName[10];
 		char contentName[10];
 		char address[80];
-	} struct pdu
+	};
+	struct pdu
 	{
 		char type;
 		char data[100];
@@ -32,17 +33,19 @@ int main(int argc, char *argv[])
 	char rbuf[100], sbuf[100]; /* "input" buffer; any size > 0	*/
 	char *pts;
 	char readPeerName[10], readContentName[10], readAddress[80];
-	int sock;							/* server socket		*/
-	time_t now;							/* current time			*/
-	int alen;							/* from-address length		*/
-	int *numContent = 0, *numUsers = 0; // how many items are in database
-	struct sockaddr_in sin;				/* an Internet endpoint address         */
-	int s, type;						/* socket descriptor and socket type    */
+	int sock;						   /* server socket		*/
+	time_t now;						   /* current time			*/
+	int alen;						   /* from-address length		*/
+	int numContent = 0, *numUsers = 0; // how many items are in database
+	struct sockaddr_in sin;			   /* an Internet endpoint address         */
+	int s, type;					   /* socket descriptor and socket type    */
 	int port = 3000;
-	int i, j, bytesRead;
+	int i, j, bytesRead, duplicate;
 	struct pdu spdu, rpdu;
+	struct content a, b, c, d, e, f, g;
 	struct stat fileStats;
-	char database[10][100][2]; // database only able to hold 10 unique users and 100 unique content name and 1 address; might need to make dynamic array
+	struct content database[7];
+	// char database[10][100][2];  //database only able to hold 10 unique users and 100 unique content name and 1 address; might need to make dynamic array
 	int fileSize;
 	FILE *filePointer;
 
@@ -89,7 +92,19 @@ int main(int argc, char *argv[])
 			strncpy(readAddress, rpdu.data + 20, 80);
 
 			// put for loop into a function to call
-			if (checkInDatabase(readPeerName, readContentName))
+			// if (checkInDatabase(readPeerName, readContentName, database))
+			for (i = 0; i < 7; i++)
+			{
+				if (strcmp(readPeerName, database[i].peerName) != 0)
+				{ // strcmp output 0 if true
+					if (strcmp(readContentName, *database[i].contentName) != 0)
+					{
+						duplicate = 1;
+					}
+				}
+				duplicate = 0;
+			}
+			if (duplicate)
 			{
 				spdu.type = 'E';
 				strcpy(spdu.data, "Error: Content already registered");
@@ -99,18 +114,101 @@ int main(int argc, char *argv[])
 			}
 			else
 			{
-				if ((*numContent < 100) && (*numUsers < 10))
-				{
-					database[*numUsers][0][0] = readPeerName;
-					database[*numUsers][*numContent][0] = readContentName;
-					database[*numUsers][*numContent][1] = readAddress;
+				switch (numContent)
+				{ // this will help create new struct content
+				case 0:
+					strcpy(a.peerName, readPeerName);
+					strcpy(a.contentName, readContentName);
+					strcpy(a.address, readAddress);
+					database[numContent] = a;
+					numContent++;
+					spdu.type = 'A';
+					strcpy(spdu.data, "Success");
+					sendto(s, &spdu, sizeof(spdu), 0,
+						   (struct sockaddr *)&fsin, sizeof(fsin));
+					break;
+				case 1:
+					strcpy(b.peerName, readPeerName);
+					strcpy(b.contentName, readContentName);
+					strcpy(b.address, readAddress);
+					database[numContent] = b;
+					numContent++;
+					spdu.type = 'A';
+					strcpy(spdu.data, "Success");
+					sendto(s, &spdu, sizeof(spdu), 0,
+						   (struct sockaddr *)&fsin, sizeof(fsin));
+					break;
+				case 2:
+					strcpy(c.peerName, readPeerName);
+					strcpy(c.contentName, readContentName);
+					strcpy(c.address, readAddress);
+					database[numContent] = c;
+					numContent++;
+					spdu.type = 'A';
+					strcpy(spdu.data, "Success");
+					sendto(s, &spdu, sizeof(spdu), 0,
+						   (struct sockaddr *)&fsin, sizeof(fsin));
+					break;
+				case 3:
+					strcpy(d.peerName, readPeerName);
+					strcpy(d.contentName, readContentName);
+					strcpy(d.address, readAddress);
+					database[numContent] = d;
+					numContent++;
+					spdu.type = 'A';
+					strcpy(spdu.data, "Success");
+					sendto(s, &spdu, sizeof(spdu), 0,
+						   (struct sockaddr *)&fsin, sizeof(fsin));
+					break;
+				case 4:
+					strcpy(e.peerName, readPeerName);
+					strcpy(e.contentName, readContentName);
+					strcpy(e.address, readAddress);
+					database[numContent] = e;
+					numContent++;
+					spdu.type = 'A';
+					strcpy(spdu.data, "Success");
+					sendto(s, &spdu, sizeof(spdu), 0,
+						   (struct sockaddr *)&fsin, sizeof(fsin));
+					break;
+				case 5:
+					strcpy(f.peerName, readPeerName);
+					strcpy(f.contentName, readContentName);
+					strcpy(f.address, readAddress);
+					database[numContent] = f;
+					numContent++;
+					spdu.type = 'A';
+					strcpy(spdu.data, "Success");
+					sendto(s, &spdu, sizeof(spdu), 0,
+						   (struct sockaddr *)&fsin, sizeof(fsin));
+					break;
+				case 6:
+					strcpy(g.peerName, readPeerName);
+					strcpy(g.contentName, readContentName);
+					strcpy(g.address, readAddress);
+					database[numContent] = g;
+					numContent++;
+					spdu.type = 'A';
+					strcpy(spdu.data, "Success");
+					sendto(s, &spdu, sizeof(spdu), 0,
+						   (struct sockaddr *)&fsin, sizeof(fsin));
+					break;
+				default:
+					spdu.type = 'E';
+					strcpy(spdu.data, "Error: No more space for new content");
+					printf("%s\n", spdu.data);
+					sendto(s, &spdu, sizeof(spdu), 0,
+						   (struct sockaddr *)&fsin, sizeof(fsin));
+					break;
 				}
-				// database[i] = //add content into database
 
-				spdu.type = 'A';
-				strcpy(spdu.data, "Success");
-				sendto(s, &spdu, sizeof(spdu), 0,
-					   (struct sockaddr *)&fsin, sizeof(fsin));
+				// if ((*numContent < 100) && (*numUsers < 10))
+				// {
+				// 	database[*numUsers][0][0] = readPeerName;
+				// 	database[*numUsers][*numContent][0] = readContentName;
+				// 	database[*numUsers][*numContent][1] = readAddress;
+				// }
+				// database[i] = //add content into database
 
 				// create tcp socket
 			}
@@ -179,20 +277,20 @@ int main(int argc, char *argv[])
 	}
 }
 
-int checkInDatabase(char readPeerName[], char readContentName[])
-{
-	for (i = 0; i < 10; i++)
-	{
-		if (strcmp(readPeerName, database[i][0][0].peerName) != 0)
-		{ // strcmp output 0 if true
-			for (j = 0; j < 100; j++)
-			{
-				if (strcmp(readContentName, database[i][j][0].contentName) != 0)
-				{
-					return 1;
-				}
-			}
-		}
-	}
-	return 0;
-}
+// int checkInDatabase(char readPeerName[], char readContentName[], struct content *database[])
+// {
+// 	int i;
+// 	char peerName[10], contentName[10];
+// 	for (i = 0; i < 7; i++)
+// 	{
+// 		strcpy(peerName, database[i].peerName);
+// 		if (strcmp(readPeerName, peerName) != 0)
+// 		{ // strcmp output 0 if true
+// 			if (strcmp(readContentName, *database[i].contentName) != 0)
+// 			{
+// 				return 1;
+// 			}
+// 		}
+// 	}
+// 	return 0;
+// }
